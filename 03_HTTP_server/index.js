3bd0fa9bfdf05f6260c7b3efc20a5ab42927d38e
Mohit <mohit22600@gmail.com>
2026-02-08 12:00:30 +0530

    http server

--- 03_HTTP_server/index.js
+++ 03_HTTP_server/index.js
@@ -1,16 +1,30 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
 
 const myserver = http.createServer((req, res) => {
-    const log = `${Date.now()}: ${req.url} : new request recieved\n`
+    const log = `${Date.now()}: ${req.method} :${req.url} : new request recieved\n`
+    const myUrl = url.parse(req.url,true)
+    console.log(myUrl)
     fs.appendFile("./log.txt",log,(err,data) => {
-        switch(req.url){
+        switch(myUrl.pathname){
             case ("/"): res.end("This is home page")
                         break;
             case ("/about"): res.end("This is about page")
                         break;
             case ("/contactus"): res.end("This is contact page")
                         break;
+            case ("/search"): 
+                        const search= myUrl.query.search
+                        console.log(search)
+                        res.end(`Here are your result ${search}`)
+                        break;            
+            case ("/signup"):
+                        if(req.method==="GET") res.end("This is sign up page")
+                        else if(req.method==="POST"){
+                            //write code here to put data in database
+                            req.end("Success")
+                    }
         }
     })
 })
